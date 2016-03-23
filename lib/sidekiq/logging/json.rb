61504d1ad460cac585dd35fda4d8308c07b7a72b
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-03-22 17:38:29 -0700

    Convert logs to new Logstash format (no @fields)
    In the current logstash format, the only @ prefixed fields are supposed
    to be @timestamp and @version. Data fields are now at the root of the
    object rather than under @fields.
    
    Also removed redundant data from message field.

--- lib/sidekiq/logging/json.rb
+++ lib/sidekiq/logging/json.rb
@@ -10,23 +10,21 @@ module Sidekiq
         def call(severity, time, program_name, message)
           {
             '@timestamp' => time.utc.iso8601,
-            '@fields' => {
-              :pid => ::Process.pid,
-              :tid => "TID-#{Thread.current.object_id.to_s(36)}",
-              :context => "#{context}",
-              :program_name => program_name,
-              :worker => "#{context}".split(" ")[0]
-            },
-            '@type' => 'sidekiq',
-            '@status' => nil,
-            '@severity' => severity,
-            '@run_time' => nil,
-            '@message' => "#{time.utc.iso8601} #{::Process.pid} TID-#{Thread.current.object_id.to_s(36)}#{context} #{severity}: #{message}",
+            :pid => ::Process.pid,
+            :thread_id => "TID-#{Thread.current.object_id.to_s(36)}",
+            :context => "#{context}",
+            :program_name => program_name,
+            :worker => "#{context}".split(" ")[0],
+            :type => 'sidekiq',
+            :status => nil,
+            :severity => severity,
+            :run_time => nil,
+            :status_message => "#{message}",
           }.merge(process_message(severity, time, program_name, message)).to_json + "\n"
         end
 
         def process_message(severity, time, program_name, message)
-          return { '@status' => 'exception' } if message.is_a?(Exception)
+          return { :status => 'exception' } if message.is_a?(Exception)
 
           if message.is_a? Hash
             if message["retry"]
@@ -37,8 +35,8 @@ module Sidekiq
               msg = "#{message['class']} failed with args #{message['args']}, not retrying."
             end
             return {
-              '@status' => status,
-              '@message' => "#{time.utc.iso8601} #{::Process.pid} TID-#{Thread.current.object_id.to_s(36)}#{context} #{severity}: #{msg}"
+              :status => status,
+              :status_message => "#{msg}"
             }
           end
 
@@ -46,8 +44,8 @@ module Sidekiq
           status = result[0].match(/^(start|done|fail):?$/) || []
 
           {
-            '@status' => status[1],                                   # start or done
-            '@run_time' => status[1] && result[1] && result[1].to_f   # run time in seconds
+            status: status[1],                                   # start or done
+            run_time: status[1] && result[1] && result[1].to_f   # run time in seconds
           }
         end
       end
